4f6aaf1bfd33ab0838bca44a8336bcfd936f2f76
totallynotharry <162619834+totallynotharry@users.noreply.github.com>
2026-01-08 18:54:16 +0000

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
-var sitename = "native."; // Change this to change the name of your website.
-var subtext = "v1.2"; // set the subtext
+var sitename = "banana arcade"; // Change this to change the name of your website.
+var subtext = "the new banana arcade is here!"; // set the subtext
 
 // more settings in main.css
 
